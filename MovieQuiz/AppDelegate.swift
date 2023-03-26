696f83d924128bdefb4efcd211d69b4af9bb2165
Nikita <123371636+tsomuk@users.noreply.github.com>
2023-03-26 22:14:33 +0300

    sleep() method deleted

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,6 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        sleep(2)
         return true
     }
 
